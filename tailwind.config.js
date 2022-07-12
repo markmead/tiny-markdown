5950922489e671d81f352c180496ba4969502cfe
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-07-12 08:29:05 +0100

    feat/revisions (#2)
    * Init
    
    * Update - Create revision on load
    
    * Update - New UI
    
    * Update - Clean up
    
    * Update - Dont create revision on load
    
    * Update - Remove $storage plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
